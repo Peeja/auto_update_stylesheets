15795c65904e3d712e34233dfb67861af1561261
Peter Jaros <peeja@dropio.com>
2009-04-08 14:00:09 -0400

    Don't conflict with non-jQuery frameworks.

--- auto_update_stylesheets.js
+++ auto_update_stylesheets.js
@@ -1,37 +1,39 @@
-$.autoUpdateStylesheets = function() {
-  var linkElements = $('head link'), index = -1;
+(function($) {
+  $.autoUpdateStylesheets = function() {
+    var linkElements = $('head link'), index = -1;
 
-  normalizeQueryStrings();
-  compareNextStyleSheet();
+    normalizeQueryStrings();
+    compareNextStyleSheet();
 
-  function compareNextStyleSheet() {
-    var link = getNextLink(),
-        url = link.href.replace(/timestamp=\d+/, 'timestamp=' + new Date().getTime());
+    function compareNextStyleSheet() {
+      var link = getNextLink(),
+          url = link.href.replace(/timestamp=\d+/, 'timestamp=' + new Date().getTime());
 
-    $.ajax({
-      url: url,
-      success: function(data) { 
-        if (data != $(link).data('cachedData')) {
-          $(link).data('cachedData', data).attr('href', url);
+      $.ajax({
+        url: url,
+        success: function(data) { 
+          if (data != $(link).data('cachedData')) {
+            $(link).data('cachedData', data).attr('href', url);
+          }
+        }, 
+        complete: function() {
+          setTimeout(compareNextStyleSheet, 100);
         }
-      }, 
-      complete: function() {
-        setTimeout(compareNextStyleSheet, 100);
-      }
-    });
-  }
+      });
+    }
 
-  function getNextLink() {
-    index = (index + 1) % linkElements.length;
-    return linkElements[index];
-  }
+    function getNextLink() {
+      index = (index + 1) % linkElements.length;
+      return linkElements[index];
+    }
 
-  function normalizeQueryStrings() {
-    linkElements.each(function(i, link) {
-      var url = link.href;
-      if (url.indexOf('?') == -1) url += '?';
-      url += '&timestamp=0000';
-      $(link).attr('href', url);
-    });
+    function normalizeQueryStrings() {
+      linkElements.each(function(i, link) {
+        var url = link.href;
+        if (url.indexOf('?') == -1) url += '?';
+        url += '&timestamp=0000';
+        $(link).attr('href', url);
+      });
+    }
   }
-}
+})(jQuery)
